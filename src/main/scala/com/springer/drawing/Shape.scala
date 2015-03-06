24a13acf9a0a9a8e14b23c36136438dcc39a26b7
toby.weston <toby.weston@db.com>
2015-03-06 12:12:01 +0000

    what's clearer, a for-comprehension or desugared nested loop?

--- src/main/scala/com/springer/drawing/Shape.scala
+++ src/main/scala/com/springer/drawing/Shape.scala
@@ -6,16 +6,20 @@ trait Shape {
 
 class Line(start: Coordinate, end: Coordinate) extends Shape {
   def applyTo(canvas: Canvas): Unit = {
-    for (x <- start.x to end.x; y <- start.y to end.y) {
-      canvas.drawCharacter(Coordinate(x, y), 'x')
+    val line = for {
+      x <- start.x to end.x
+      y <- start.y to end.y
+    } yield {
+      Coordinate(x, y)
     }
+    line.foreach(canvas.drawCharacter(_, 'x'))
   }
 }
 
 object Test extends App {
   val canvas = Canvas(10, 4)
   new Line(Coordinate(1, 2), Coordinate(6, 2)).applyTo(canvas)
+  new Line(Coordinate(6, 3), Coordinate(6, 4)).applyTo(canvas)
   println(canvas.toString)
 }
-
 
