8c248c44e7212e03e90de6fa368cf709fd25ce02
toby.weston <toby.weston@db.com>
2015-03-06 15:39:48 +0000

    example App that follows the PDF's example (acceptance criteria)

---
+++ src/test/scala/com/springer/drawing/Example.scala
@@ -0,0 +1,10 @@
+package com.springer.drawing
+
+object Example extends App {
+  val canvas = ArrayCanvas(20, 4)
+  Line((1, 2), (6, 2)).applyTo(canvas)
+  Line((6, 3), (6, 4)).applyTo(canvas)
+  Rectangle((16, 1), (20, 3)).applyTo(canvas)
+
+  println(canvas)
+}
